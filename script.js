4a8ecac42d461b6cdffddb4689a02b81a6221be6
Rida-e-Fatima <126771470+Rida-e-Fatima@users.noreply.github.com>
2023-06-12 09:24:59 +0500

    portfolio

---
+++ script.js
@@ -0,0 +1,12 @@
+//AOS Link
+AOS.init();
+
+//Navbar 
+const navbar = document.getElementById("main-navbar");
+window.addEventListener('scroll', function() {
+  if (window.pageYOffset > 0) {
+    navbar.classList.add("navbar-after-scroll");
+  } else {
+    navbar.classList.remove("navbar-after-scroll");
+  }
+});
